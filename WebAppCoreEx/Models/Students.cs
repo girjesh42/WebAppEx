f18241ee83145b04b08ef0392dd661b6be16bbd3
girjesh42 <kumargirjesh2442@gmail.com>
2021-03-16 15:52:49 +0530

    first and final

---
+++ WebAppCoreEx/Models/Students.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WebAppCoreEx.Models
+{
+    public partial class Students
+    {
+        public int Sid { get; set; }
+        public string Sname { get; set; }
+        public int Sage { get; set; }
+        public string Course { get; set; }
+        public string Semail { get; set; }
+        public string Scontact { get; set; }
+    }
+}
---
+++ WebAppCoreEx/Models/StudentsDay39Context.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WebAppCoreEx.Models
+{
+    public partial class StudentsDay39Context : DbContext
+    {
+        public StudentsDay39Context()
+        {
+        }
+
+        public StudentsDay39Context(DbContextOptions<StudentsDay39Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Students> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+//            if (!optionsBuilder.IsConfigured)
+//            {
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+//                optionsBuilder.UseSqlServer("Server=GIRJESH-HP-BS18;database=StudentsDay39;trusted_connection=true;");
+//            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Students>(entity =>
+            {
+                entity.HasKey(e => e.Sid)
+                    .HasName("PK__Students__CA1E5D7847FF93DA");
+
+                entity.Property(e => e.Sid).ValueGeneratedNever();
+
+                entity.Property(e => e.Course).HasMaxLength(50);
+
+                entity.Property(e => e.Scontact).HasMaxLength(10);
+
+                entity.Property(e => e.Semail)
+                    .IsRequired()
+                    .HasMaxLength(100);
+
+                entity.Property(e => e.Sname)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
